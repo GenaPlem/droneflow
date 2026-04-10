23c865e1f958087dd4e4b3e703e9bac7b7ea1b16
GenaPlem <shainkill1337@gmail.com>
2026-04-10 19:07:20 +0100

    Update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -75,8 +75,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
-    url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
+    url: "http://localhost:3000/projects",
+    reuseExistingServer: true,
     timeout: 120 * 1000,
   },
 });
